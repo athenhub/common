cfccf5eadd1e4b74eee15fa226e087b5a19211b8
JKjiwon <jwkim.oa@gmail.com>
2025-11-13 23:18:52 +0900

    [FIX] `@SpringBootTest` 설정 변경

---
+++ src/test/java/com/athenhub/common/TestLibraryConfig.java
@@ -0,0 +1,9 @@
+package com.athenhub.common;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.athenhub.common")
+public class TestLibraryConfig {
+}
