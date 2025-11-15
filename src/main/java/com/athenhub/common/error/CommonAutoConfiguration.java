1ba86f708740d18795d67cf97f3d2ff06cee1c95
JKjiwon <jwkim.oa@gmail.com>
2025-11-15 14:45:54 +0900

    REMOVE: 기존 `@RestControllerAdvice` 삭제

--- src/main/java/com/athenhub/common/error/CommonAutoConfiguration.java
+++ src/main/java/com/athenhub/common/error/CommonAutoConfiguration.java
@@ -1,24 +1,10 @@
 package com.athenhub.common.error;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.context.annotation.Bean;
 
-@AutoConfiguration // 자동 구성 설정 클래스임을 명시
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET) // 이 자동 구성 전체가 웹 환경일 때만 활성화되도록 조건을 추가 (필수)
+@AutoConfiguration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class CommonAutoConfiguration {
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ExceptionResolver exceptionResolver() {
-        return new ExceptionResolver();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public CommonErrorAdvice commonErrorAdvice() {
-        return new CommonErrorAdvice();
-    }
 
 }
